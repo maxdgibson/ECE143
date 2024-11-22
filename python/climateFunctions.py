b810e26e26f2c3c20e6c68a89c46ab15c3da1fca
Ashwin agesh <ashwinagesh@gmail.com>
2024-11-21 20:43:55 -0800

    Add comments and resolve conflicts

--- python/climateFunctions.py
+++ python/climateFunctions.py
@@ -2,26 +2,31 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-def getMonthLocationOverYears(df, species_id, tracking_month, year_range):
+def getMonthLocationOverYears(df, species_id, tracking_month, year_range, year_step):
     '''
     Filter the dataframe location of a species in a particular month for all years in the specified range.
 
     Inputs:
     df             :- full dataframe
-    species_id     :- species to track
+    species_id     :- list of species to track
     tracking_month :- month to filter by
     year_range     :- start and end year (inclusive)
+    year_step      :- year increment step size
 
     Output :- filtered dataframe
     '''
-    species_data = df[df['SPECIES_ID'] == species_id]
+    if species_id:
+        species_data = df[df['SPECIES_ID'].isin(species_id)]    
+    else:
+        species_data = df
     species_data['MONTH_YEAR'] = pd.to_datetime(species_data['EVENT_YEAR'].astype(str) + '-' + species_data['EVENT_MONTH'].astype(str), errors='coerce')
 
     migration_data = species_data[['MONTH_YEAR', 'EVENT_MONTH', 'EVENT_YEAR', 'LAT_DD', 'LON_DD']].dropna()
     month_migration = migration_data[migration_data['EVENT_MONTH'] == tracking_month]
-    month_migration = month_migration[month_migration['EVENT_YEAR'] >= year_range[0]]
-    month_migration = month_migration[month_migration['EVENT_YEAR'] <= year_range[1]]
+    year_lst = [y for y in range(year_range[0], year_range[1]+1, year_step)]
+    month_migration = month_migration[month_migration['EVENT_YEAR'].isin(year_lst)]
     return month_migration
+
 
 def calcCentroidPerYear(month_migration):
     '''
@@ -34,6 +39,7 @@ def calcCentroidPerYear(month_migration):
     '''
     yearly_centroids = (month_migration.groupby('MONTH_YEAR')[['EVENT_YEAR', 'LAT_DD', 'LON_DD']].mean().reset_index().sort_index())
     return yearly_centroids
+
 
 def plotCentroids(world_shape, yearly_centroids):
     '''
@@ -49,7 +55,6 @@ def plotCentroids(world_shape, yearly_centroids):
     world = world[world['CONTINENT'] == 'North America']
     ax = world.plot(figsize=(15, 10), color='lightgrey')
     # Superimpose bird sightings
-    ax.plot(yearly_centroids['LON_DD'], yearly_centroids['LAT_DD'], color='blue', linewidth=1, markersize=4, label="Path of Movement")
     ax.text(yearly_centroids['LON_DD'][0], yearly_centroids['LAT_DD'][0], "Start", fontsize=10, color='green', fontweight='bold')
     ax.text(yearly_centroids['LON_DD'].iloc[-1], yearly_centroids['LAT_DD'].iloc[-1], "End", fontsize=10, color='red', fontweight='bold')
     for index, centroid in yearly_centroids.iterrows():
@@ -61,7 +66,23 @@ def plotCentroids(world_shape, yearly_centroids):
     plt.legend(title="Year")
     plt.show()
 
+
 def getFirstSighting(df, species_id, latitudes, year_range, year_step, tracking_month):
+    '''
+    This function groups first sightings of a species between specified latitudes in the month
+    under consideration.
+    Median of the first 10 sightings is used to reduce noise and avoid abberant sightings.
+
+    Inputs:
+    df             :- full dataframe
+    species_id     :- species to track
+    latitude       :- latitude ranges to group first sightings
+    year_range     :- start and end year (inclusive)
+    year_step      :- year increment step size
+    tracking_month :- month to filter by
+
+    Output :- filtered dataframe
+    '''
     if species_id:
         species_data = df[df['SPECIES_ID'].isin(species_id)]    
     else:
