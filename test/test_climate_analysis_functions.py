aae79677420626863613d4a34243c79bfc54f90e
Chandrakant I Viraktamath <chandan.v2@gmail.com>
2024-12-06 19:04:50 -0800

    Add climate analysis results and minor script corrections

--- test/test_climate_analysis_functions.py
+++ test/test_climate_analysis_functions.py
@@ -1,15 +1,15 @@
-from ../climateAnalysis import bird_mov_map_gen, temp_heat_map_gen, stich_images_as_gif
+from climateAnalysis import bird_mov_map_gen, temp_heat_map_gen, stich_images_as_gif
 
 def main():
     #Input Parameters
     
     #Path containing input tavg csv file scrubbed using automate_temp_data_scrubbing.py file
-    inp_path = r'D:\PythonCodePractice\Python_Project\TAVG_DataSet\'
+    inp_path = r'<path_to_dataset_organized_as_csv_file_using_automate_temp_data_scrubbing.py'
     start_year = 1960
     end_year = 2020
     duration_gap = 5
     months_to_analyse = [4,5,6]
-    bird_data_file = r'D:\PythonCodePractice\Python_Project\TAVG_DataSet\goose.pkl'
+    bird_data_file = r'pkl_file_containing_bird_centroid_movement_data' #Sample provided in /datasets/migration/ path
     region_bounds = [-94, -78, 35, 50]
 
     print("Executing bird map gen test")
